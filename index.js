de64bd2d8ed457a39c90cf90beb46ab3b64314d4
yueyueyangyang <53545498+yueyueyangyang@users.noreply.github.com>
2022-10-13 10:13:23 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+    // 给gitbook注入自定义的css和js文件
+    website: {
+        assets: './assets',
+        js: [
+            'base.js'
+        ],
+        css: [
+            'style.css'
+        ]
+    },
+    // Map of hooks 一些钩子函数
+    hooks: {},
+
+    // Map of new blocks
+    blocks: {},
+
+    // Map of new filters
+    filters: {}
+};
